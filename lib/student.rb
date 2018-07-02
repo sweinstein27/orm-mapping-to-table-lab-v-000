edee8814879d47866be36f768d6616c2ce8ca92f
Scott Weinstein <sweinstein27@gmail.com>
2018-07-02 16:42:34 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,13 +11,13 @@ end
   #  with DB[:conn]
 
   def self.create_table
-    sql = <<-SQL 
+    sql = <<-SQL
     CREATE TABLE IF NOT EXISTS songs (
       id INTEGER PRIMARY KEY,
       name TEXT,
       grade NUMBER
     )
-    SQL 
+    SQL
 
   end
 
