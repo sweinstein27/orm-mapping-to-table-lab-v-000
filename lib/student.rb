8ba1b1c052076968ee1373f3a7d57e0e05e0d1e4
Scott Weinstein <sweinstein27@gmail.com>
2018-07-02 16:39:17 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,7 +10,9 @@ end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
-  def self.create_table
+  def self.create_table(name:, grade:)
+    
+    
   end
 
 
